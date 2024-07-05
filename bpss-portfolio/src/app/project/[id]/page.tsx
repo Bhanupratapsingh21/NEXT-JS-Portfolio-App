f1a463ff96508bcc69013b56a79fd89117e74442
bhanupratapsingh21 <bhanupss137@gmail.com>
2024-07-05 22:04:17 +0530

    bug fix-1

--- bpss-portfolio/src/app/project/[id]/page.tsx
+++ bpss-portfolio/src/app/project/[id]/page.tsx
@@ -10,23 +10,25 @@ interface Data {
   description: string;
   techstack: string;
   images: { name: string }[];
-  link : string;
-  livelink : string
+  link: string;
+  livelink: string
 }
 
 
 const Project = () => {
 
   const params = useParams();
-  const [id,setid] = useState<number>(Number(params.id))
-  if(id >= db.length+1){
+  const [id, setid] = useState<number>(Number(params.id));
+  const [data, setData] = useState<Data>(db[id - 1]);
+  
+  if (id >= db.length + 1) {
     return (
       <div className='text-center px-56 py-56'>
-      404 | Not Found  
+        404 | Not Found
       </div>
     )
   }
-  const [data, setData] = useState<Data>(db[id-1]);
+
   const testimonials = data.images.map(image => ({
     quote: data.name,
     name: image.name,
@@ -37,9 +39,9 @@ const Project = () => {
       <div>
         <div className='flex justify-between'>
           <h1 className="text-3xl text-left mt-16 mb-6 md:text-5xl">{data.name}</h1>
-          <button onClick={()=> window.location.href = data.link} className=" md:h-14 md:w-28 w-24 h-16 mt-[69px] md:mt-[54px] mb-6 overflow-hidden shadow-[0_0_0_3px_#000000_inset] md:px-4 md:py-2 bg-transparent border border-gray-500  text-white rounded-lg font-bold transform hover:-translate-y-1 text-center transition duration-400">
-                                        Git-Repo
-                                    </button>
+          <button onClick={() => window.location.href = data.link} className=" md:h-14 md:w-28 w-24 h-16 mt-[69px] md:mt-[54px] mb-6 overflow-hidden shadow-[0_0_0_3px_#000000_inset] md:px-4 md:py-2 bg-transparent border border-gray-500  text-white rounded-lg font-bold transform hover:-translate-y-1 text-center transition duration-400">
+            Git-Repo
+          </button>
         </div>
         <p className="mt-4 text-left  text-base/6 text-neutral-200">
           {data.description}
@@ -49,15 +51,15 @@ const Project = () => {
         <hr />
         <div className='flex justify-between'>
           <h1 className="text-3xl text-left mt-16 mb-6 md:text-5xl">Preview</h1>
-          <button onClick={()=> window.location.href = data.livelink} className=" md:w-28 md:h-14 w-24 h-10 mt-16 md:mt-[54px] mb-6 overflow-hidden shadow-[0_0_0_3px_#000000_inset] md:px-4 md:py-2 bg-transparent border border-gray-500  text-white rounded-lg font-bold transform hover:-translate-y-1 text-center transition duration-400">
-                                        Live-Link
-                                    </button>
+          <button onClick={() => window.location.href = data.livelink} className=" md:w-28 md:h-14 w-24 h-10 mt-16 md:mt-[54px] mb-6 overflow-hidden shadow-[0_0_0_3px_#000000_inset] md:px-4 md:py-2 bg-transparent border border-gray-500  text-white rounded-lg font-bold transform hover:-translate-y-1 text-center transition duration-400">
+            Live-Link
+          </button>
         </div>
       </div>
 
-      <div  onClick={()=> window.location.href = data.livelink }>
+      <div onClick={() => window.location.href = data.livelink}>
         <InfiniteMovingCards
-          
+
           items={testimonials}
           direction="right"
           speed="slow"
